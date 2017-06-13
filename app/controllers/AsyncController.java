a05801dc2ab4e4c11f4b4c60c5a13a64034e78ef
Ilya Ivanina <ilya.ivanina@gmail.com>
2017-06-13 17:31:44 +0300

    Asynchronous Handling
    1 - Handling asynchronous results
    2 - Streaming HTTP responses
    3 - Comet  (do not compleat)
    4 - WebSockets
    
    WebSockets - for test: https://www.websocket.org/echo.html
    using ws://localhost:9000/socket-test

--- app/controllers/AsyncController.java
+++ app/controllers/AsyncController.java
@@ -1,11 +1,24 @@
 package controllers;
 
+import akka.NotUsed;
 import akka.actor.ActorSystem;
+
 import javax.inject.*;
 
 import akka.actor.Scheduler;
+import akka.actor.Status;
+import akka.stream.OverflowStrategy;
+import akka.stream.javadsl.Source;
+import akka.util.ByteString;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import play.*;
+import play.libs.Comet;
+import play.libs.Json;
+import play.libs.concurrent.HttpExecutionContext;
 import play.mvc.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.concurrent.Executor;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
@@ -26,24 +39,27 @@ public class AsyncController extends Controller {
     private final ActorSystem actorSystem;
     private final ExecutionContextExecutor exec;
 
+    @Inject
+    HttpExecutionContext ec;
+
     /**
      * @param actorSystem We need the {@link ActorSystem}'s
-     * {@link Scheduler} to run code after a delay.
-     * @param exec We need a Java {@link Executor} to apply the result
-     * of the {@link CompletableFuture} and a Scala
-     * {@link ExecutionContext} so we can use the Akka {@link Scheduler}.
-     * An {@link ExecutionContextExecutor} implements both interfaces.
+     *                    {@link Scheduler} to run code after a delay.
+     * @param exec        We need a Java {@link Executor} to apply the result
+     *                    of the {@link CompletableFuture} and a Scala
+     *                    {@link ExecutionContext} so we can use the Akka {@link Scheduler}.
+     *                    An {@link ExecutionContextExecutor} implements both interfaces.
      */
     @Inject
     public AsyncController(ActorSystem actorSystem, ExecutionContextExecutor exec) {
-      this.actorSystem = actorSystem;
-      this.exec = exec;
+        this.actorSystem = actorSystem;
+        this.exec = exec;
     }
 
     /**
      * An action that returns a plain text message after a delay
      * of 1 second.
-     *
+     * <p>
      * The configuration in the <code>routes</code> file means that this method
      * will be called when the application receives a <code>GET</code> request with
      * a path of <code>/message</code>.
@@ -54,12 +70,119 @@ public class AsyncController extends Controller {
 
     private CompletionStage<String> getFutureMessage(long time, TimeUnit timeUnit) {
         CompletableFuture<String> future = new CompletableFuture<>();
+
         actorSystem.scheduler().scheduleOnce(
-            Duration.create(time, timeUnit),
-            () -> future.complete("Hi!"),
-            exec
+                Duration.create(time, timeUnit),
+                () -> future.complete("Hi!"),
+                exec
         );
         return future;
     }
+
+
+    public CompletionStage<Result> testAction() {
+        return CompletableFuture
+                .supplyAsync(() -> {
+                    //TODO: do something
+                    try {
+                        Thread.sleep(4000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return new HashMap<String, String>() {{
+                        put("timeout", "4 sec");
+                        put("k1", "value1");
+                        put("k2", "value2");
+                    }};
+                })
+                .thenApply(i -> ok(Json.toJson(i)));
+    }
+
+    public CompletionStage<Result> testCompletionStage() {
+        String cookeName = "test-cookie-counter";
+        return CompletableFuture.supplyAsync(() -> {
+            Http.Cookie cookie = request().cookie(cookeName);
+            int count = 0;
+            if (cookie != null) {
+                Logger.debug("cookie exists");
+                count += Integer.parseInt(cookie.value()) + 1;
+            }
+            Logger.debug("count: " + count);
+            cookie = new Http.Cookie(cookeName, count + "", 100, "/", "localhost", false, false);
+
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Logger.debug("set cookie: " + cookie.name() + " with value: " + cookie.value());
+            response().setCookie(cookie);
+            return count;
+        }, ec.current())
+                .thenApply(i -> ok("count: " + i));
+    }
+
+
+    public Result chunkedTest() {
+        // Prepare a chunked text stream
+        Source<ByteString, ?> source = Source.<ByteString>actorRef(256, OverflowStrategy.dropNew())
+                .mapMaterializedValue(sourceActor -> {
+                    sourceActor.tell(ByteString.fromString(" ->kiki"), null);
+                    sourceActor.tell(ByteString.fromString(" ->foo"), null);
+                    sourceActor.tell(ByteString.fromString(" ->bar"), null);
+
+                    // request not finished without next line
+                    sourceActor.tell(new Status.Success(NotUsed.getInstance()), null);
+                    return null;
+                });
+        // Serves this stream with 200 OK
+        return ok().chunked(source);
+    }
+
+
+    /**
+     * Do not work correctly (as need)
+     * https://www.playframework.com/documentation/2.5.x/JavaComet
+     */
+    public Result cometTest() {
+        final ObjectNode objectNode = Json.newObject();
+        objectNode.put("foo", "bar-1");
+        objectNode.put("foo-2", "bar-2");
+        final Source source = Source.from(Arrays.asList(objectNode));
+
+        return ok().chunked(
+                source.via(
+                        Comet.json("console.log")
+                ))
+                .as(Http.MimeTypes.HTML);
+    }
+
+
+    //---------------------------------------------
+
+
+    /**
+     * Handling WebSockets using callbacks
+     *
+     * for test use: https://www.websocket.org/echo.html
+     * and ws://localhost:9000/socket-test
+     */
+    public LegacyWebSocket<String> socketTest() {
+        return WebSocket.whenReady((in, out) -> {
+            // For each event received on the socket,
+            in.onMessage(System.out::println);
+
+            in.onMessage(msg -> {
+                out.write("-=("+msg+")=-");
+            });
+
+            // When the socket is closed.
+            in.onClose(() -> System.out.println("Disconnected"));
+
+            // Send a single 'Hello!' message
+            out.write("Hello!");
+        });
+    }
+
 
 }
